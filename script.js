14b81ed26a96ee4478c8c9e870faca4b6195455a
vishalkashyap1155 <vishalkashyap1155@gmail.com>
2025-07-29 21:46:37 +0530

    all files are added in update

---
+++ script.js
@@ -0,0 +1,54 @@
+let btn = document.getElementById("btn");
+let listContainer = document.querySelector(".listContainer");
+
+function clickBtn(){
+
+    let input = document.querySelector("#input");
+
+    if(input.value == ""){
+        alert("Enter your text first")
+    }else{
+        
+    let li = document.createElement("LI")
+
+    li.innerHTML = input.value;
+
+    listContainer.appendChild(li);
+
+    input.value = "";
+
+    let span = document.createElement("SPAN");
+
+    span.innerHTML = "x"
+    li.appendChild(span)
+    
+    }
+
+    savedata();
+
+}
+
+listContainer.addEventListener("click" ,(e)=>{
+
+
+    if(e.target.tagName == "LI"){
+       e.target.classList.toggle("checked")
+    }
+
+    
+    if(e.target.tagName == "SPAN"){
+       e.target.parentElement.remove()
+    }
+    savedata()
+
+})
+
+function savedata(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showdata(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+
+showdata();
